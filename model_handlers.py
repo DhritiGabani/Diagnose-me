deccc27e8ef54f0702c47fb3ed8e7c9fb61d49de
DhritiGabani <dhritigabani@gmail.com>
2023-03-27 07:28:35 -0400

    fix typo

--- model_handlers.py
+++ model_handlers.py
@@ -2,7 +2,7 @@ import librosa
 from tensorflow.keras.models import load_model
 import joblib
 import numpy as np
-import pandas as p
+import pandas as pd
 import sklearn
 from sklearn.ensemble import RandomForestClassifier
 import cv2
@@ -24,24 +24,27 @@ def respiratory_model(audio_path):
         return predictions
     else:
         return "Cannot process audio file"
-    
+
+
 def heart_disease_model(data):
-    model = joblib.load("./static/models/heart-disease-detection-model/model.joblib")
+    model = joblib.load(
+        "./static/models/heart-disease-detection-model/model.joblib")
     X_test = np.array(data)
 
-    df = pd.DataFrame(data = X_test).T
+    df = pd.DataFrame(data=X_test).T
     if '' not in X_test:
         prediction = model.predict(df)
         print(prediction[0])
 
     return prediction[0]
 
+
 def parkinson_model(image_path):
     model = load_model(
-            "static/models/parkinsons-detection-model/parkinson-model.h5")
+        "static/models/parkinsons-detection-model/parkinson-model.h5")
     img = cv2.imread(image_path)
-    img = cv2.resize(img, (200, 200)) # Resizing the images
-    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB) # Converting the image into RBG
+    img = cv2.resize(img, (200, 200))  # Resizing the images
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)  # Converting the image into RBG
     img = np.expand_dims(img, axis=0)
     prediction = model.predict(img)
     return np.argmax(prediction)
